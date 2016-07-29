2bcb56a32c2a83ff179c9dd5905ce02e2ed3476e
Saito Nakamura <saitonakamuramb@gmail.com>
2016-07-29 13:56:58 +0300

    start

---
+++ StructuredLogging/WithLogging/Application.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Autofac;
+using Autofac.Extras.DynamicProxy2;
+using Castle.DynamicProxy;
+using NUnit.Framework;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace StructuredLogging.WithLogging
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var classWithVirtualMethodsToLog = IocContainer.Container.Resolve<ClassWithVirtualMethodsToLog>();
+
+            classWithVirtualMethodsToLog.SomeMethodToLog();
+        }
+    }
+
+    public static class IocContainer
+    {
+        static IocContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterType<LoggerInterceptor>();
+            containerBuilder.RegisterType<ClassWithVirtualMethodsToLog>()
+                .EnableClassInterceptors().InterceptedBy(typeof(LoggerInterceptor));
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://docker.local:9200")) { AutoRegisterTemplate = true })
+                .CreateLogger();
+
+            Log.Logger = logger;
+
+            Container = containerBuilder.Build();
+        }
+
+        public static IContainer Container { get; set; }
+    }
+
+    public class LoggerInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            var methodExecutionTimer = new Stopwatch();
+
+            methodExecutionTimer.Start();
+
+            invocation.Proceed();
+
+            methodExecutionTimer.Stop();
+
+            var methodName = invocation.Method.Name;
+            var methodExecutionTime = methodExecutionTimer.Elapsed;
+
+            Log.Information("Method '{methodName}' is called, {methodExecutionTime} elapsed", methodName, methodExecutionTime);
+        }
+    }
+
+    public class ClassWithVirtualMethodsToLog
+    {
+        public virtual void SomeMethodToLog()
+        {
+            Thread.Sleep(1234);
+        }
+    }
+}
---
+++ StructuredLogging/WithoutLogging/ApplicationWithoutLogging.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using Autofac;
+using NUnit.Framework;
+
+namespace StructuredLogging.WithoutLogging
+{
+    [TestFixture]
+    public class TestsWithoutLogging
+    {
+        [Test]
+        public void Test1()
+        {
+            var classWithVirtualMethodsToLog = IocContainerWithoutLogging.Container.Resolve<ClassWithVirtualMethodsToLogWithoutLogging>();
+
+            classWithVirtualMethodsToLog.SomeMethodToLog();
+        }
+    }
+
+    public static class IocContainerWithoutLogging
+    {
+        static IocContainerWithoutLogging()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterType<ClassWithVirtualMethodsToLogWithoutLogging>();
+
+            Container = containerBuilder.Build();
+        }
+
+        public static IContainer Container { get; set; }
+    }
+
+    public class ClassWithVirtualMethodsToLogWithoutLogging
+    {
+        public void SomeMethodToLog()
+        {
+            Thread.Sleep(1234);
+        }
+    }
+}
